8c8963dd2ab33a4214af8153685b48340ba8df89
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-10 01:00:14 -0500

    add search for city box and populate the following 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,163 +1,128 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
-    //EventListner--onclick. Grab value from input box to API
+//EventListner--onclick. Grab value from input box to API
 
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-    //Fetch data from the API. Current weather and one call  API. api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    //city name= var name
+//Fetch data from the API. Current weather and one call  API. api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//city name= var name
 
 // WHEN I view current weather conditions for that city
- //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-    //API- feth and 2 .then-console log 
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//API- feth and 2 .then-console log 
 
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-    //grab lon/lat from Current Weather and passing it to One Call. Create a var that represents lon/lat to fetch info from One call and
-    //append this data to my page.
+//grab lon/lat from Current Weather and passing it to One Call. Create a var that represents lon/lat to fetch info from One call and
+//append this data to my page.
 
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-    //Also, append this information to page. 
+//Also, append this information to page. 
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-    // need a local storage to store searched cities. Grab stuff from local storage and display in page. 
+// need a local storage to store searched cities. Grab stuff from local storage and display in page. 
 
 
 var myAPI = "8ce43231e9bfeecbefc30eede9ec8820";
-var fiveDay;
-var cityName = "austin";
+var fiveDayURL;
 var part = "minutely, hourly";
+var cityName;
 
-var oneCall;
+var oneCallURL;
 
+// Makes a network call to the Open Weather API to download the weather for the specified cityName.
+function getCoords(cityName) {
+    // String Interpolation
+    fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPI}`;
 
-function getCoords() {
-    fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPI}`;
-
-
-
-    fetch(fiveDay)
-        .then(function(response) {
+    fetch(fiveDayURL)
+        .then(function (response) {
             if (response.ok) {
                 response.json()
-                .then(function(data) {
-                    console.log(data);
-                    var latitude = data.city.coord.lat;
-                    
-                       var longitude = data.city.coord.lon;
-                       console.log("latitude " + latitude);
-                       console.log("longitude " + longitude);
-                       getOneDay(latitude, longitude);
-
-    clearDiv("temp1");
-    clearDiv("temp2");
-    clearDiv("temp3");
-    clearDiv("temp4");
-    clearDiv("temp5");
-
-                })
-            }
-            
-        })
-    }
-
-getCoords();
-        
-
-    function getOneDay(latitude, longitude) {
-        oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=${part}&appid=${myAPI}`;
-
-        //Fetch the API URL.
-        fetch(oneCall)
-            //Then run the fn with response.
-            .then(function(response) {
-                
-                if (response.ok) {
-                    console.log(response);
-                    //Convert response to JSON
-                    response.json()
-                    .then(function(data) {
+                    .then(function (data) {
                         console.log(data);
-                         
-                        var todaysDate = (moment.unix(data.daily[0].dt).format("MM/DD/YYYY"));
-                         console.log(todaysDate);
-                         var currentCity = cityName;
-                         console.log(currentCity);
-                         var currentWind = data.current.wind_speed;
-                         console.log(currentWind);
-                         var currentUVI = data.current.uvi;
-                         console.log(currentUVI);
-                         var currentTemp = data.current.temp;
-                         console.log(currentTemp);
-                         var currentHumidity = data.current.humidity;
-                         console.log(currentHumidity);
-                         var icon = data.current.weather[0].icon;
-                         console.log(icon);
-                         var currentIcon = "http://openweathermap.org/img/wn/" + icon + ".png";
-                         console.log(currentIcon);
-                         day2(latitude, longitude);          
-                    })
-
-                }
-
-            })
-        }
-
-        function day2(latitude, longitude) {
-            oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=${part}&appid=${myAPI}`;
-    
-            //Fetch the API URL.
-            fetch(oneCall)
-                //Then run the fn with response.
-                .then(function(response) {
-                    
-                    if (response.ok) {
-                        console.log(response);
-                        //Convert response to JSON
-                        response.json()
-                        .then(function(data) {
-                            console.log(data);
-                            for (let i = 1; i <= 5; i++){
-   
-                           
-                            var date2 = (moment.unix(data.daily[i].dt).format("MM/DD/YYYY"));
-                             console.log(date2);
-                             var wind2 = data.daily[i].wind_speed;
-                             console.log(wind2);
-                            
-                             var temp2 = data.daily[i].temp.max;
-                             console.log(temp2);
-                             var humidity2 = data.daily[i].humidity;
-                             console.log(humidity2);
-                             var icon2 = data.daily[i].weather[0].icon;
-                             console.log(icon2);
-                             var icon2a = "http://openweathermap.org/img/wn/" + icon2 + ".png";
-                             console.log(icon2a);
-                             var temperature = "temp" + [i];
-                             console.log(temperature);
-                             var day2_add = document.createElement('li');
-          
-            day2_add.textContent = date2;
-                           document.getElementById(temperature).appendChild(day2_add);
-              var addImg = document.createElement('img');
-              addImg.src = icon2a;
-              addImg.alt = "Weather Icon";
-              document.getElementById(temperature).appendChild(addImg);
-
-                            } 
-                            })
-
+                        var latitude = data.city.coord.lat;
+                        var longitude = data.city.coord.lon;
+                        console.log("latitude " + latitude);
+                        console.log("longitude " + longitude);
+                        getDailyDays(latitude, longitude);
+                        for (var i = 1; i < 6; i++) {
+                            clearDiv("temp" + i);
                         }
-        
                     })
-                }
+            }
+        })
+}
+
+function getDailyDays(latitude, longitude) {
+    oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=${part}&appid=${myAPI}`;
+
+    //Fetch the API URL.
+    fetch(oneCallURL)
+        //Then run the fn with response.
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                //Convert response to JSON
+                response.json()
+                    .then(function (data) {
+                        console.log(data);
+                        for (let i = 0; i < 6; i++) {
+                            // dt is the unix time stamp we are converting to an actual date format.
+                            var date = (moment.unix(data.daily[i].dt).format("MM/DD/YYYY"));
+                            console.log(date);
+                            var windSpeed = data.daily[i].wind_speed;
+                            console.log(windSpeed);
+                            var maxTemp = data.daily[i].temp.max;
+                            console.log(maxTemp);
+                            var humidity = data.daily[i].humidity;
+                            console.log(humidity);
+                            // Every weather contains an array of only 1 element.
+                            var iconName = data.daily[i].weather[0].icon;
+                            console.log(iconName);
+                            var iconURL = "http://openweathermap.org/img/wn/" + iconName + ".png";
+                            console.log(iconURL);
+
+                            if (i == 0) {
+                                // Add the first day on the top container.
+                            } else {
+                                // Add the other days as small containers.
+                                var temperature = "temp" + i;
+                                console.log(temperature);
+                                var dayListItemEl = document.createElement('li');
+                                dayListItemEl.textContent = date;
+                                document.getElementById(temperature).appendChild(dayListItemEl);
+                                var addImg = document.createElement('img');
+                                addImg.src = iconURL;
+                                addImg.alt = "Weather Icon";
+                                document.getElementById(temperature).appendChild(addImg);
+                            }
+                        }
+                    })
+            }
+        })
+}
 
 
-                function clearDiv(elementID) {
-                    var ul = document.getElementById(elementID);
-                    while(ul.firstChild) {
-                        ul.removeChild(ul.firstChild);
-                    }
-                }
-                        
+function clearDiv(elementID) {
+    var ul = document.getElementById(elementID);
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+}
+
+var searchButton = document.getElementById("searchButton");
+function searchButtonClicked() {
+    // Gets the text inside the input box.
+    var city = document.getElementById("cityInputBox").value;
+    if (city === "") {
+        console.log("User did not enter a city.");
+    } else {
+        console.log("User entered the city " + city + ".");
+        cityName = city;
+        // Call the API with the city.
+        getCoords(city);
+    }
+}
+searchButton.addEventListener("click", searchButtonClicked);
