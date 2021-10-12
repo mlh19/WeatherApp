efb3b577bb21f396cba0ec7ba4231c7590f46e0d
Mary Lou <ml.hoover@yahoo.com>
2021-10-12 01:15:52 -0500

    fix styling issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
 
 // WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//API- feth and 2 .then-console log 
+//API- feth and 2 .then-console log
 
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
@@ -17,228 +17,229 @@
 
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//Also, append this information to page. 
+//Also, append this information to page.
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-// need a local storage to store searched cities. Grab stuff from local storage and display in page. 
-
+// need a local storage to store searched cities. Grab stuff from local storage and display in page.
 
 var myAPI = "8ce43231e9bfeecbefc30eede9ec8820";
 var fiveDayURL;
 var part = "minutely, hourly";
 var cityName;
-
 var oneCallURL;
+
 
 // Makes a network call to the Open Weather API to download the weather for the specified cityName.
 function getCoords(cityName) {
-    // String Interpolation
-    fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPI}`;
+  // String Interpolation
+  fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPI}`;
 
-    fetch(fiveDayURL)
-        .then(function (response) {
-            if (response.ok) {
-                saveCity(cityName);
-                response.json()
-                    .then(function (data) {
-                        console.log(data);
-                        var latitude = data.city.coord.lat;
-                        var longitude = data.city.coord.lon;
-                        console.log("latitude " + latitude);
-                        console.log("longitude " + longitude);
-                        getDailyDays(latitude, longitude);
-                        for (var i = 1; i < 6; i++) {
-                            clearDiv("temp" + i);
-                        }
-                    })
-            }
-        })
+  fetch(fiveDayURL).then(function (response) {
+    if (response.ok) {
+      saveCity(cityName);
+      response.json().then(function (data) {
+        console.log(data);
+        var latitude = data.city.coord.lat;
+        var longitude = data.city.coord.lon;
+        console.log("latitude " + latitude);
+        console.log("longitude " + longitude);
+        getDailyDays(latitude, longitude);
+        for (var i = 1; i < 6; i++) {
+          clearDiv("temp" + i);
+        }
+      });
+    }
+  });
 }
 
 function getDailyDays(latitude, longitude) {
-    oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=${part}&appid=${myAPI}`;
-    //console.log(oneCallURL);
+  oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=${part}&appid=${myAPI}`;
+  //console.log(oneCallURL);
 
-    //Fetch the API URL.
-    fetch(oneCallURL)
-        //Then run the fn with response.
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
-                //Convert response to JSON
-                response.json()
-                    .then(function (data) {
-                        console.log(data);
-                        for (let i = 0; i < 6; i++) {
-                            // dt is the unix time stamp we are converting to an actual date format.
-                            var date = (moment.unix(data.daily[i].dt).format("MM/DD/YYYY"));
-                            console.log(date);
-                            var windSpeed = data.daily[i].wind_speed;
-                            console.log(windSpeed);
-                            var maxTemp = data.daily[i].temp.max;
-                            console.log(maxTemp);
-                            var humidity = data.daily[i].humidity;
-                            console.log(humidity);
-                            // Every weather contains an array of only 1 element.
-                            var iconName = data.daily[i].weather[0].icon;
-                            console.log(iconName);
-                            var iconURL = "http://openweathermap.org/img/wn/" + iconName + ".png";
-                            console.log(iconURL);
+  //Fetch the API URL.
+  fetch(oneCallURL)
+    //Then run the fn with response.
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        //Convert response to JSON
+        response.json().then(function (data) {
+          console.log(data);
+          for (let i = 0; i < 6; i++) {
+            // dt is the unix time stamp we are converting to an actual date format.
+            var date = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+            console.log(date);
+            var windSpeed = data.daily[i].wind_speed;
+            console.log(windSpeed);
+            var maxTemp = data.daily[i].temp.max;
+            console.log(maxTemp);
+            var humidity = data.daily[i].humidity;
+            console.log(humidity);
+            // Every weather contains an array of only 1 element.
+            var iconName = data.daily[i].weather[0].icon;
+            console.log(iconName);
+            var iconURL =
+              "http://openweathermap.org/img/wn/" + iconName + ".png";
+            console.log(iconURL);
 
-                            if (i == 0) {
-                                // TODO: Add the first day on the top container.
-                                // const forecastUlEl = document.getElementById("");
+            if (i == 0) {
+              var forecastCurrentUlID = "temp" + i;
+              const forecastCurrentUlEl = document.getElementById(forecastCurrentUlID);
 
-                            //     var dateLabel = document.createElement('li');
-                            //     dateLabel.textContent = date;
-                            //     forecastUlEl.appendChild(dateLabel);
+              var dateLabel = document.createElement("li");
+              dateLabel.textContent = date;
+              forecastCurrentUlEl.appendChild(dateLabel);
 
-                            //     var addImg = document.createElement('img');
-                            //     addImg.src = iconURL;
-                            //     addImg.alt = "Weather Icon";
-                            //     forecastUlEl.appendChild(addImg);
+              var addImg = document.createElement("img");
+              addImg.src = iconURL;
+              addImg.alt = "Weather Icon";
+              forecastCurrentUlEl.appendChild(addImg);
 
-                            //     //Temp
-                            //     var tempLabel = document.createElement('li');
-                            //     tempLabel.textContent = "Temp: " + maxTemp + " \xB0F";
-                            //     forecastUlEl.appendChild(tempLabel);
+              //Temp
+              var tempLabel = document.createElement("li");
+              tempLabel.textContent = "Temp: " + maxTemp + " \xB0F";
+              forecastCurrentUlEl.appendChild(tempLabel);
 
-                            //     //Wind
-                            //     var windLabel = document.createElement('li');
-                            //     windLabel.textContent = "Wind: " +windSpeed + " MPH";
-                            //     forecastUlEl.appendChild(windLabel);
+              //Wind
+              var windLabel = document.createElement("li");
+              windLabel.textContent = "Wind: " + windSpeed + " MPH";
+              forecastCurrentUlEl.appendChild(windLabel);
 
-                            //     //Humidity
-                            //     var humidityLabel = document.createElement('li');
-                            //     humidityLabel.textContent = "Humidity: " + humidity + "%";
-                            //     forecastUlEl.appendChild(humidityLabel);
-                               
-                            //    //UVI
-                            //     var uvi = data.daily[i].uvi;
+              //Humidity
+              var humidityLabel = document.createElement("li");
+              humidityLabel.textContent = "Humidity: " + humidity + "%";
+              forecastCurrentUlEl.appendChild(humidityLabel);
 
+              // UVI
+              var uvi = data.daily[i].uvi;
+              var uviLabel = document.createElement("li");
+              uviLabel.textContent = "UV Index: " + uvi;
+              forecastCurrentUlEl.appendChild(uviLabel);
 
-                            } else {
-                                // Add the other days as small containers.
-                                var forecastUlID = "temp" + i;
-                                const forecastUlEl = document.getElementById(forecastUlID);
+            } else {
+              // Add the other days as small containers.
+              var forecastUlID = "temp" + i;
+              const forecastUlEl = document.getElementById(forecastUlID);
 
-                                var dateLabel = document.createElement('li');
-                                dateLabel.textContent = date;
-                                forecastUlEl.appendChild(dateLabel);
+              var dateLabel = document.createElement("li");
+              dateLabel.textContent = date;
+              forecastUlEl.appendChild(dateLabel);
 
-                                var addImg = document.createElement('img');
-                                addImg.src = iconURL;
-                                addImg.alt = "Weather Icon";
-                                forecastUlEl.appendChild(addImg);
+              var addImg = document.createElement("img");
+              addImg.src = iconURL;
+              addImg.alt = "Weather Icon";
+              forecastUlEl.appendChild(addImg);
 
-                                //Temp
-                                var tempLabel = document.createElement('li');
-                                tempLabel.textContent = "Temp: " + maxTemp + " \xB0F";
-                                forecastUlEl.appendChild(tempLabel);
+              //Temp
+              var tempLabel = document.createElement("li");
+              tempLabel.textContent = "Temp: " + maxTemp + " \xB0F";
+              forecastUlEl.appendChild(tempLabel);
 
-                                //Wind
-                                var windLabel = document.createElement('li');
-                                windLabel.textContent = "Wind: " + windSpeed + " MPH";
-                                forecastUlEl.appendChild(windLabel);
+              //Wind
+              var windLabel = document.createElement("li");
+              windLabel.textContent = "Wind: " + windSpeed + " MPH";
+              forecastUlEl.appendChild(windLabel);
 
-                                //Humidity
-                                var humidityLabel = document.createElement('li');
-                                humidityLabel.textContent = "Humidity: " + humidity + "%";
-                                forecastUlEl.appendChild(humidityLabel);
-
-                            }
-                        }
-                    })
+              //Humidity
+              var humidityLabel = document.createElement("li");
+              humidityLabel.textContent = "Humidity: " + humidity + "%";
+              forecastUlEl.appendChild(humidityLabel);
             }
-        })
+          }
+        });
+      }
+    });
 }
 
-
 function clearDiv(elementID) {
-    var ul = document.getElementById(elementID);
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
+  var ul = document.getElementById(elementID);
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
 }
 
 // Search Button Section
 var searchButton = document.getElementById("searchButton");
 searchButton.setAttribute("class", "btn btn-info");
-searchButton.setAttribute("style", "margin-bottom: 5px"); 
+searchButton.setAttribute("style", "margin-bottom: 5px");
 function searchButtonClicked() {
-    // Gets the text inside the input box.
-    var city = document.getElementById("cityInputBox").value;
-    if (city === "") {
-        console.log("User did not enter a city.");
-    } else {
-        console.log("User entered the city " + city + ".");
-        cityName = city;
-        // Call the API with the city.
-        getCoords(city);
-    }
+  // Gets the text inside the input box.
+  var city = document.getElementById("cityInputBox").value;
+  if (city === "") {
+    console.log("User did not enter a city.");
+  } else {
+    console.log("User entered the city " + city + ".");
+    cityName = city;
+    // Call the API with the city.
+    getCoords(city);
+  }
 }
 searchButton.addEventListener("click", searchButtonClicked);
 
 // Local Storage Section
 function saveCity(cityName) {
-    // Convert the inputted cityName to capitalized case to avoid saving cities with different casing.
-    const upperCaseCity = cityName.toUpperCase();
-    // Implement the saving of the cityName using local storage.
-    localStorage.setItem("WeatherApp-" + upperCaseCity, upperCaseCity);
-    console.log("Saved city: " + upperCaseCity + ".");
+  // Convert the inputted cityName to capitalized case to avoid saving cities with different casing.
+  const upperCaseCity = cityName.toUpperCase();
+  // Implement the saving of the cityName using local storage.
+  localStorage.setItem("WeatherApp-" + upperCaseCity, upperCaseCity);
+  console.log("Saved city: " + upperCaseCity + ".");
 
-    // Reload so that the new city we added gets its own button.
-    loadCitiesFromStorage();
+  // Reload so that the new city we added gets its own button.
+  loadCitiesFromStorage();
 }
 
 // Loads all the cities from local storage and creates a button for that city.
 function loadCitiesFromStorage() {
-    // Get all the keys for local storage, and then ONLY get the ones that follow the filtering which
-    // is if the key contains "WeatherApp-".
-    var validKeys = Object.keys(localStorage).filter(word => word.includes("WeatherApp-") === true);
-    console.log(validKeys);
+  // Get all the keys for local storage, and then ONLY get the ones that follow the filtering which
+  // is if the key contains "WeatherApp-".
+  var validKeys = Object.keys(localStorage).filter(
+    (word) => word.includes("WeatherApp-") === true
+  );
+  console.log(validKeys);
 
-    // The index to reference each key by.
-    var numOfKeys = validKeys.length;
-    var amountOfButtons = Math.min(numOfKeys, 8);
+  // The index to reference each key by.
+  var numOfKeys = validKeys.length;
+  var amountOfButtons = Math.min(numOfKeys, 8);
 
-    // Clear any existing butons before we re-add them.
-    const divEl = document.getElementById("previousSearchButtons");
-    while (divEl.firstChild) {
-        divEl.removeChild(divEl.firstChild);
-    }
+  // Clear any existing butons before we re-add them.
+  const divEl = document.getElementById("previousSearchButtons");
+  while (divEl.firstChild) {
+    divEl.removeChild(divEl.firstChild);
+  }
 
-    // Create buttons for all of the cities it finds in local storage.
-    for (var i = 0; i < amountOfButtons; i++) {
-        // Create a button element.
-        const button = document.createElement("button");
-        button.setAttribute("class", "btn btn-secondary");
-        button.setAttribute("style", "margin-bottom: 5px" );
-        // The getItem is giving me the city name for the current key for this application only.
-        button.textContent = localStorage.getItem(validKeys[i]);
-        console.log("Found " + button.textContent + " in local storage.");
-        // Attach the function to that search button we just created.
-        button.addEventListener("click", previousSearchButtonClicked);
-        // Add that button to the div to display it.
-        divEl.append(button);
-    }
+  // Create buttons for all of the cities it finds in local storage.
+  for (var i = 0; i < amountOfButtons; i++) {
+    // Create a button element.
+    const button = document.createElement("button");
+    button.setAttribute("class", "btn btn-outline-secondary");
+    button.setAttribute("style", "margin-bottom: 5px");
+    // The getItem is giving me the city name for the current key for this application only.
+    button.textContent = localStorage.getItem(validKeys[i]);
+    console.log("Found " + button.textContent + " in local storage.");
+    // Attach the function to that search button we just created.
+    button.addEventListener("click", previousSearchButtonClicked);
+    // Add that button to the div to display it.
+    divEl.append(button);
+  }
 }
 
 // Remove all the cities from local storage.
 function removeCitiesFromStorage() {
-    const validKeys = Object.keys(localStorage).filter(word => word.includes("WeatherApp-") === true);
-    const numofKeys = validKeys.length;
-    for (var i = 0; i < numofKeys; i++) {
-        localStorage.removeItem(validKeys[i]);
-    }
+  const validKeys = Object.keys(localStorage).filter(
+    (word) => word.includes("WeatherApp-") === true
+  );
+  const numofKeys = validKeys.length;
+  for (var i = 0; i < numofKeys; i++) {
+    localStorage.removeItem(validKeys[i]);
+  }
 }
 
 function previousSearchButtonClicked() {
-    // "this" refers to the button that CALLED this function.
-    // Get the city from the button's text content.
-    console.log(this.textContent);
-    getCoords(this.textContent);
+  // "this" refers to the button that CALLED this function.
+  // Get the city from the button's text content.
+  console.log(this.textContent);
+  getCoords(this.textContent);
 }
 
 loadCitiesFromStorage();
