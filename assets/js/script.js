5f9dc33f11426dd5f78a78b8241c999be4079c17
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-12 10:21:51 -0500

    fix a bug where the top container repeated data if the button was clicked more than once. Also make changes to formatting.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-//EventListner--onclick. Grab value from input box to API
-
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-//Fetch data from the API. Current weather and one call  API. api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//city name= var name
-
-// WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//API- feth and 2 .then-console log
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//grab lon/lat from Current Weather and passing it to One Call. Create a var that represents lon/lat to fetch info from One call and
-//append this data to my page.
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//Also, append this information to page.
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// need a local storage to store searched cities. Grab stuff from local storage and display in page.
-
 var myAPI = "8ce43231e9bfeecbefc30eede9ec8820";
 var fiveDayURL;
 var part = "minutely, hourly";
@@ -45,7 +20,7 @@ function getCoords(cityName) {
         console.log("latitude " + latitude);
         console.log("longitude " + longitude);
         getDailyDays(latitude, longitude);
-        for (var i = 1; i < 6; i++) {
+        for (var i = 0; i < 6; i++) {
           clearDiv("temp" + i);
         }
       });
